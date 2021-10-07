de623b609b6692e8811af7a1ac2b8c6902c1ebd5
Yongrok <94chl@naver.com>
2021-10-07 14:05:59 +0900

    visit count

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,9 +6,9 @@ export default function Footer({ $target }) {
   this.render = () => {
     $header.innerHTML = `
     <div>v0.42.0(21.09.29)</div>
-    <div>Copyright and intellectual property rights regarding all games belong to "Nimble Neuron"
-    corp</div>
+    <div>Copyright and intellectual property rights regarding all games belong to "Nimble Neuron"corp</div>
     <div>created by BrownRoomFish</div>
+    <div><img src="https://hitcounter.pythonanywhere.com/count/tag.svg?url=https%3A%2F%2F94chl.github.io%2F01RK%2F" alt="Hits"></div>
     `;
   };
 

7669dc4794e55f6c6ad01d3acef75c3634911282
Yongrok <94chl@naver.com>
2021-10-13 20:23:56 +0900

    hits change

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -8,7 +8,7 @@ export default function Footer({ $target }) {
     <div>v0.42.0(21.09.29)</div>
     <div>Copyright and intellectual property rights regarding all games belong to "Nimble Neuron"corp</div>
     <div>created by BrownRoomFish</div>
-    <div><img src="https://hitcounter.pythonanywhere.com/count/tag.svg?url=https%3A%2F%2F94chl.github.io%2F01RK%2F" alt="Hits"></div>
+    <div><a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2F94chl.github.io%2F01RK%2F&count_bg=%23FFBB00&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false"/></a></div>
     `;
   };
 
